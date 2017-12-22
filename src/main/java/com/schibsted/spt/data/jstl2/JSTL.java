6a25278ec3b54e0f9bcb9e90737c652b321487d5
Lars Marius Garshol <lars.marius.garshol@schibsted.com>
2017-12-22 16:08:19 +0100

    Command-line improvements

--- src/main/java/com/schibsted/spt/data/jstl2/JSTL.java
+++ src/main/java/com/schibsted/spt/data/jstl2/JSTL.java
@@ -10,13 +10,17 @@ public class JSTL {
 
   public static void main(String[] args) throws Exception {
     Expression expr = Parser.compile(new File(args[0]));
-    ((ExpressionImpl) expr).dump();
+    if (expr instanceof ExpressionImpl)
+      ((ExpressionImpl) expr).dump();
 
     ObjectMapper mapper = new ObjectMapper();
 
     JsonNode input = mapper.readTree(new File(args[1]));
 
     JsonNode output = expr.apply(input);
+
+    if (output == null)
+      System.out.println("WARN: returned Java null!");
 
     System.out.println(mapper.writerWithDefaultPrettyPrinter()
                        .writeValueAsString(output));
