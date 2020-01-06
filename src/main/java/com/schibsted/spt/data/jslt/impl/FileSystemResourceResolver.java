c9c598f4e9d0fdafdb7064731004b2f96b805a6d
Lars Marius Garshol <larsga@garshol.priv.no>
2020-01-06 15:19:33 +0100

    Move away from java.io.nio and simplify code

--- src/main/java/com/schibsted/spt/data/jslt/impl/FileSystemResourceResolver.java
+++ src/main/java/com/schibsted/spt/data/jslt/impl/FileSystemResourceResolver.java
@@ -1,14 +1,13 @@
 
 package com.schibsted.spt.data.jslt.impl;
 
+import java.io.File;
 import java.io.Reader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
@@ -16,35 +15,40 @@ import com.schibsted.spt.data.jslt.JsltException;
 import com.schibsted.spt.data.jslt.ResourceResolver;
 
 public final class FileSystemResourceResolver implements ResourceResolver {
-  private Path rootPath;
+  private File rootPath; // can be null
   private Charset charset;
 
+  public FileSystemResourceResolver(File rootPath, Charset charset) {
+    this.rootPath = rootPath;
+    this.charset = charset;
+  }
+
+  public FileSystemResourceResolver(File rootPath) {
+    this(rootPath, StandardCharsets.UTF_8);
+  }
+
   public FileSystemResourceResolver(Path rootPath, Charset charset) {
-    this.rootPath = rootPath.toAbsolutePath();
+    this.rootPath = rootPath.toAbsolutePath().toFile();
     this.charset = charset;
   }
 
   public FileSystemResourceResolver(Path rootPath) {
-    this(rootPath, StandardCharsets.UTF_8);
+    this(rootPath.toFile(), StandardCharsets.UTF_8);
   }
 
   public FileSystemResourceResolver(Charset charset) {
-    this(FileSystems.getDefault().getPath("").toAbsolutePath(), charset);
+    this((File) null, charset);
   }
 
   public FileSystemResourceResolver() {
-    this(FileSystems.getDefault().getPath("").toAbsolutePath(), StandardCharsets.UTF_8);
+    this((File) null, StandardCharsets.UTF_8);
   }
 
   @Override
   public Reader resolve(String jslt) {
     try {
-      Path jsltPath = FileSystems.getDefault().getPath(jslt).normalize();
-      if (!jsltPath.isAbsolute()) {
-        jsltPath = rootPath.resolve(jsltPath).normalize();
-      }
-
-      InputStream is = Files.newInputStream(jsltPath, StandardOpenOption.READ);
+      File file = new File(rootPath, jslt);
+      InputStream is = new FileInputStream(file);
       return new InputStreamReader(is, charset);
     } catch (IOException e) {
       throw new JsltException("Could not resolve file '" + jslt + "': " + e, e);
--- src/test/java/com/schibsted/spt/data/jslt/FileSystemResourceResolverTest.java
+++ src/test/java/com/schibsted/spt/data/jslt/FileSystemResourceResolverTest.java
@@ -9,85 +9,75 @@ import com.fasterxml.jackson.databind.node.NullNode;
 import com.schibsted.spt.data.jslt.impl.FileSystemResourceResolver;
 import org.junit.Test;
 
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
-public final class FileSystemResourceResolverTest {
+public class FileSystemResourceResolverTest {
+  private static final ObjectMapper mapper = new ObjectMapper();
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+  @Test
+  public void testResolveImportsFromFilesystem() throws IOException {
+    FileSystemResourceResolver resolver = new FileSystemResourceResolver();
+    Expression e = parse("./src/test/resources/import-from-fs/working1.jslt", resolver);
+    assertEquals(
+      readResource("import-from-fs/working1_expected_result.json"),
+      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(e.apply(mapper.readTree("{}"))));
+  }
 
-    @Test
-    public void testResolveImportsFromFilesystem() throws IOException  {
-        FileSystemResourceResolver resolver = new FileSystemResourceResolver();
-        Path jslt = FileSystems.getDefault().getPath("./src/test/resources/import-from-fs/working1.jslt");
-        Expression e = new Parser(
-          new InputStreamReader(new FileInputStream(jslt.toFile()))
-        )
-          .withResourceResolver(resolver)
-          .compile();
-        assertEquals(
-                readResource("import-from-fs/working1_expected_result.json"),
-                mapper.writerWithDefaultPrettyPrinter().writeValueAsString(e.apply(mapper.readTree("{}"))));
+  @Test
+  public void testResolveImportsFromFilesystemWithExplicitRootPath() throws IOException {
+    FileSystemResourceResolver resolver =
+      new FileSystemResourceResolver(new File("src/test/resources/import-from-fs"));
+    Expression e = parse("./src/test/resources/import-from-fs/working2.jslt", resolver);
+    assertEquals(
+      readResource("import-from-fs/working1_expected_result.json"),
+      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(e.apply(mapper.readTree("{}"))));
+  }
+
+  @Test
+  public void testResolveImportsFromFilesystemNotWorking() throws IOException {
+    FileSystemResourceResolver resolver = new FileSystemResourceResolver();
+    try {
+      parse("./src/test/resources/import-from-fs/wrong_relative_path.jslt", resolver);
+      fail("Expected " + JsltException.class.getSimpleName());
+    } catch (final JsltException e) {
+      assertEquals(FileNotFoundException.class, e.getCause().getClass());
     }
+  }
 
-    @Test
-    public void testResolveImportsFromFilesystemWithExplicitRootPath() throws IOException  {
-        FileSystemResourceResolver resolver =
-          new FileSystemResourceResolver(FileSystems.getDefault().getPath("src/test/resources/import-from-fs"));
-        Path jslt = FileSystems.getDefault().getPath("./src/test/resources/import-from-fs/working2.jslt");
-        Expression e = new Parser(
-          new InputStreamReader(new FileInputStream(jslt.toFile()))
-        )
-          .withResourceResolver(resolver)
-          .compile();
-        assertEquals(
-                readResource("import-from-fs/working1_expected_result.json"),
-                mapper.writerWithDefaultPrettyPrinter().writeValueAsString(e.apply(mapper.readTree("{}"))));
+  @Test
+  public void testResolveImportsFromFilesystemWithEncoding() throws IOException {
+    FileSystemResourceResolver resolver = new FileSystemResourceResolver(
+      new File("./src/test/resources"), StandardCharsets.ISO_8859_1
+    );
+    Expression e = parse("./src/test/resources/character-encoding-master.jslt", resolver);
+
+    JsonNode result = e.apply(NullNode.instance);
+    assertEquals("Hei på deg", result.asText());
+  }
+
+  private Expression parse(String resource, ResourceResolver resolver) throws IOException {
+    return new Parser(
+        new FileReader(new File(resource))
+      )
+      .withResourceResolver(resolver)
+      .compile();
+  }
+
+  private String readResource(final String path) throws IOException {
+    try {
+      return new String(Files.readAllBytes(Paths.get(getClass().getClassLoader()
+                                                     .getResource(path).toURI())), StandardCharsets.UTF_8);
+    } catch (URISyntaxException e) {
+      throw new IOException(e);
     }
-
-    @Test
-    public void testResolveImportsFromFilesystemNotWorking() throws IOException  {
-        FileSystemResourceResolver resolver = new FileSystemResourceResolver();
-        Path jslt = FileSystems.getDefault().getPath("./src/test/resources/import-from-fs/wrong_relative_path.jslt");
-        try {
-            new Parser(
-              new InputStreamReader(new FileInputStream(jslt.toFile()))
-            )
-              .withResourceResolver(resolver)
-              .compile();
-            fail("Expected " + JsltException.class.getSimpleName());
-        } catch (final JsltException e) {
-            assertEquals(NoSuchFileException.class, e.getCause().getClass());
-        }
-    }
-
-    @Test
-    public void testResolveImportsFromFilesystemWithEncoding() throws IOException  {
-        FileSystemResourceResolver resolver = new FileSystemResourceResolver(
-          FileSystems.getDefault().getPath("./src/test/resources"), StandardCharsets.ISO_8859_1
-        );
-        Path jslt = FileSystems.getDefault().getPath("./src/test/resources/character-encoding-master.jslt");
-        Expression e = new Parser(
-          new InputStreamReader(new FileInputStream(jslt.toFile()))
-        )
-          .withResourceResolver(resolver)
-          .compile();
-
-        JsonNode result = e.apply(NullNode.instance);
-        assertEquals("Hei på deg", result.asText());
-    }
-
-    private String readResource(final String path) throws IOException {
-        try {
-            return new String(Files.readAllBytes(Paths.get(getClass().getClassLoader()
-                    .getResource(path).toURI())), StandardCharsets.UTF_8);
-        } catch (URISyntaxException e) {
-            throw new IOException(e);
-        }
-    }
+  }
 
 }
