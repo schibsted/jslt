7ac77e1a2b26e7164a97a9c933be7884890a2848
Lars Marius Garshol <larsga@garshol.priv.no>
2020-06-09 19:35:44 +0200

    Delete lambda subproject

--- lambda/update-lambda.sh
+++
@@ -1,6 +0,0 @@
-ARTIFACT_PATH=../build/libs/jslt*-all.jar
-ARTIFACT=`basename $ARTIFACT_PATH`
-set -e
-aws s3 cp $ARTIFACT_PATH s3://spt-data-dev-lmg/lambda/jstl2/
-aws lambda update-function-code --function-name JSTL2Demo --s3-bucket spt-data-dev-lmg --s3-key lambda/jstl2/$ARTIFACT --publish
-aws s3 cp lambda.html s3://spt-data-dev-public-web/jstl2.html
